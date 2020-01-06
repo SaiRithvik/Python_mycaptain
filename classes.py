fd1f913000fc3438cc70e380740122b22143a367
SAI RITHVIK KANAKAMEDALA <58741111+SaiRithvik@users.noreply.github.com>
2020-01-06 10:28:49 +0530

    Let's do programming

---
+++ classes.py
@@ -0,0 +1,11 @@
+class rectangle:
+	length = float(input())
+	width = float(input())
+	Area = length*width
+	def area(self):
+		print(self.Area)
+		return
+
+rectangle1 = rectangle();
+rectangle1.area();
+
